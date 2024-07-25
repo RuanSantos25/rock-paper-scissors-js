9e0eb5e92b065c84a5873b1689de498d18ac57c5
RuanSantos25 <[email protected]>
2024-07-25 14:50:55 -0300

    Add function that asks the player to type rock, paper or scissors
    The function asks the player to type one of three options (rock, paper
     and scissors) analyzing the result regardless of upper or lower case,
     using the .toLowerCase() method. If the player chooses between one of
     the three options, the value is returned by the function, if the player
     chooses anything else, null is returned.

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function getComputerChoice() {
     else if (computerChoice === 1) return "paper";
     else return "scissors";
 }
+
+function getHumanChoice() {
+    let humanChoice = prompt("rock, paper or scissors?");
+    if (humanChoice.toLowerCase() === "rock") return "rock";
+    else if (humanChoice.toLowerCase() === "paper") return "paper";
+    else if (humanChoice.toLowerCase() === "scissors") return "scissors";
+    else return null;
+}
